b41b69e725b2b05c1510ea6a7d7d8aa8fca3f0a0
Erik Thulin <Erik Thulin>
2016-06-10 11:32:00 -0400

    Adding exercise from chapter 13

---
+++ 13_classes_and_objects.py
@@ -0,0 +1,9 @@
+#Rewrite the distance function from chapter 5 so that it takes two Points as parameters instead of four numbers.
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.y = y
+        self.x = x
+
+def distance(a,b):
+    return ((a.x-b.x)**2 + (a.y-b.y)**2)**0.5
